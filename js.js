56c5528a0dbf39f73d5c446be957a23896886838
rahul rajput <rahulrajput7358@gmail.com>
2021-02-26 14:51:19 +0530

    clock

---
+++ js.js
@@ -0,0 +1,17 @@
+let hour  = document.querySelector('.hour');
+let minute  = document.querySelector('.minute');
+let second  = document.querySelector('.second');
+
+setInterval(() => {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    hRotate = 30 * h + m/2;
+    mRotate = 6 * m ;
+    sRotate = 6 * s ;
+    hour.style.transform = `rotate(${hRotate}deg)`;
+    minute.style.transform = `rotate(${mRotate}deg)`;
+    second.style.transform = `rotate(${sRotate}deg)`;
+
+}, 1000);
